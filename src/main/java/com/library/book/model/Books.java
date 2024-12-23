c456d89cefa2a5a7db6c74ae72672ca97dc36ad0
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-12-23 10:41:08 -0300

    create and map the User entity

---
+++ src/main/java/com/library/book/model/Books.java
@@ -0,0 +1,105 @@
+package com.library.book.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="books")
+public class Books implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	@Column(name= "author", nullable= false, length = 40)
+	private String autor;
+	@Column(name = "launch_date", nullable = false)
+	
+	private LocalDateTime dateTime;
+	@Column(nullable= false, length = 15)
+	private Double price;
+	@Column(name= "title", nullable= false, columnDefinition = "TEXT")
+	private String text;
+	
+	public Books() {
+		
+	}
+
+	public Books(Long id, String autor, LocalDateTime dateTime, Double price, String text) {
+		this.id = id;
+		this.autor = autor;
+		this.dateTime = dateTime;
+		this.price = price;
+		this.text = text;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(autor, dateTime, id, price, text);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Books other = (Books) obj;
+		return Objects.equals(autor, other.autor) && Objects.equals(dateTime, other.dateTime)
+				&& Objects.equals(id, other.id) && Objects.equals(price, other.price)
+				&& Objects.equals(text, other.text);
+	}
+
+	
+}
